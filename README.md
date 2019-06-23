581eae6fc86c3001a5be428970fc0a80441901f0
brettrd <brett.downing1@gmail.com>
2019-06-23 12:46:14 +1000

    take seed as argument, update readme

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ A deterministic generator of random hostnames.
 A trivial rule and a seeded random number generator allow a human-readable hostname to be associated with a given physical machine,
 or a given (re)deployment of VMs
 
-Reports probability of hostname collision with a given deployment size.
-
 The birthday problem is unkind to very large deployments, pull requests welcome to expand the lists or expand the name space.
+
+The python script reports probability of hostname collision with a given deployment size, then prints some examples.
+
+The shell scripts will generate a host from a given seed (like a mac address).  
+The script will hash the seed (md5), which allows any unique data source to be used and still effectively span a huge namespace.
+
+`change_hostname.sh $(./name_gen.sh $(cat /sys/class/net/eth0/address))`
 
--- name_gen.sh
+++ name_gen.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#find some unique entropy on the system,
-#making sure the name can be repeatably generated.
-MAC_IF="enp3s0"
-ENTR_SRC=$(cat /sys/class/net/$MAC_IF/address)
-RND_NUM=$(echo "ibase=16;$(echo $ENTR_SRC | md5sum | sed 's/-//' | tr a-z A-Z)" | bc)
+#hash the seed and convert the hex to a decimal value (string format)
+#this allows the seed to be any data source, including incrementing values and still span most of the namespace.
+RND_SEED=$1
+RND_NUM=$(echo "ibase=16;$(echo $RND_SEED | md5sum | sed 's/-//' | tr a-z A-Z)" | bc)
 #echo $RND_NUM
 
 
@@ -20,6 +19,7 @@ NAM_IDX=$(echo "($RND_NUM/$NUM_ADJ)%$NUM_NAM" | bc)
 
 NAM_STR=$(head -n$NAM_IDX fantasy_names.txt | tail -n1)
 ADJ_STR=$(head -n$ADJ_IDX adjectives.txt | tail -n1)
-RND_HOSTNAME=$(echo "$NAM_STR-the-$ADJ_STR" | tr A-Z a-a)
 RND_NAME="$NAM_STR the $ADJ_STR"
-echo "introducing $RND_NAME"
+RND_HOSTNAME=$(echo "$RND_NAME" | sed "s/ /-/g" | tr A-Z a-z)
+#echo "introducing $RND_NAME"
+echo "$RND_HOSTNAME"
