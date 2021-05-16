8e197a65b4d8e6dd29655b107fae322b67e252ad
BrettRD <brettrd@brettrd.com>
2021-05-17 01:20:13 +1000

    improve stability and add systemd service

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
-host_name=$1
-echo $host_name | tee /etc/hostname
-sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$host_name"'/' /etc/hosts
-hostnamectl set-hostname $host_name
-systemctl restart avahi-daemon
+
+new_host_name=$1
+old_host_name=$(hostname)
+
+if [ $new_host_name == $old_host_name ]
+then
+  echo "hostname looks fine, finished"
+  exit 0
+else
+  echo "old hostname is $old_host_name, changing to $new_host_name"
+
+  # change the system hostname
+  hostnamectl set-hostname --static $new_host_name
+
+  # update /etc/hosts
+  # this applies to debian systems and doesn't get touched by hostnamectl
+  sed -i -E 's/^127.0.1.1.*/127.0.1.1   '"$new_host_name"'/' /etc/hosts
+  # mop up the alias on ::1
+  sed -i -E 's/'"$old_host_name"'/'"$new_host_name"'/g' /etc/hosts
+
+  # announce on the local network as $new_host_name.local
+  systemctl restart avahi-daemon
+
+  # the machine would benefit from a reboot at this point
+
+fi
+
+exit 0
